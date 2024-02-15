86c617b0653a28fdffdc4ccecef98eaa7baeb398
Hichem Khelifi Ahmed <khel0010@algonquinlive.com>
2024-02-15 15:45:31 -0500

    Finished test cases and Javadocs

---
+++ app/src/androidTest/java/algonquin/cst2335/lab5/testFindMissingUpperCase.java
@@ -0,0 +1,38 @@
+package algonquin.cst2335.lab5;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class testFindMissingUpperCase {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testFindMissingUpperCase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
+        appCompatEditText.perform(replaceText("password123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+}
---
+++ app/src/main/java/algonquin/cst2335/lab5/MainActivity.java
@@ -0,0 +1,116 @@
+package algonquin.cst2335.lab5;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity class represents the main screen of the application.
+ * It allows users to input a password and checks its complexity.
+ *
+ * @author Hichem Khelifi Ahmed
+ * @version 1.0
+ */
+public final class MainActivity extends AppCompatActivity {
+
+    /**
+     * This holds the text displayed at the center of the screen.
+     */
+    private TextView tv = null;
+
+    /**
+     * This holds the EditText field where users input their passwords.
+     */
+    private EditText et = null;
+
+    /**
+     * This holds the Button used to trigger the password complexity check.
+     */
+    private Button btn = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        et = findViewById(R.id.editTextTextPassword);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(clk -> {
+            String password = et.getText().toString();
+            if (checkPasswordComplexity(password)) {
+                tv.setText("Your password meets the requirements");
+            } else {
+                tv.setText("You shall not pass!");
+            }
+        });
+    }
+
+    /**
+     * This function checks if the given string meets the password complexity requirements.
+     *
+     * @param pw The string object that we are checking
+     * @return Return true if the password is complex
+     */
+    boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+
+        for (int i = 0; i < pw.length(); i++) {
+            char c = pw.charAt(i);
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else if (isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        if (!foundUpperCase) {
+            Toast.makeText(this, "Missing an upper case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundLowerCase) {
+            Toast.makeText(this, "Missing a lower case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundNumber) {
+            Toast.makeText(this, "Missing a number", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecial) {
+            Toast.makeText(this, "Missing a special character", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Checks if the character is a special character.
+     *
+     * @param c The character to check
+     * @return True if the character is a special character, false otherwise
+     */
+    boolean isSpecialCharacter(char c) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '^':
+            case '&':
+            case '*':
+            case '!':
+            case '@':
+            case '?':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
