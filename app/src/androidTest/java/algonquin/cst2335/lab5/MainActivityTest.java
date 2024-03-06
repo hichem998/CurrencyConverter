671df61e6f9dad57a981aafa022be55203a69df2
Hichem Khelifi Ahmed <khel0010@algonquinlive.com>
2024-03-05 19:19:41 -0500

    Added Javadoc comments and finished 5 test cases.

--- app/src/androidTest/java/algonquin/cst2335/lab5/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/lab5/MainActivityTest.java
@@ -2,7 +2,6 @@ package algonquin.cst2335.lab5;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -19,6 +18,7 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
+
 public class MainActivityTest {
 
     @Rule
@@ -26,9 +26,9 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
+    public void testFindMissingUpperCase() {
         ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
-        appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("password123#$*"));
 
         ViewInteraction materialButton = onView(withId(R.id.button));
         materialButton.perform(click());
@@ -36,4 +36,53 @@ public class MainActivityTest {
         ViewInteraction textView = onView(withId(R.id.textView));
         textView.check(matches(withText("You shall not pass!")));
     }
+
+    @Test
+    public void testFindMissingLowerCase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
+        appCompatEditText.perform(replaceText("PASSWORD123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testFindMissingNumeric() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
+        appCompatEditText.perform(replaceText("PasswordABC#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testFindMissingSpecialCharacter() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
+        appCompatEditText.perform(replaceText("Password123ABC"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testValidPassword() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editTextTextPassword));
+        appCompatEditText.perform(replaceText("ValidPassword123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("Your password meets the requirements")));
+    }
+
 }
